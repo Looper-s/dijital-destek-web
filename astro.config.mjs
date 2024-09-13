344aa2b2519f0eb28179340914d30a414b83a5b2
Ali İhsan Şepar <aliihsansepar@gmail.com>
2024-09-13 18:22:22 +0300

    prod ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://looper-s.github.io/dijital-destek-web/",
-  base: "/dijital-destek-web>",
+  site: "https://looper-s.github.io",
+  base: "/dijital-destek-web",
 });
